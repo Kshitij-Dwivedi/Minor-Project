63b9b4073a0ec96eed724bc9daeb1d735b765917
Kshitij-Dwivedi <68234512+Kshitij-Dwivedi@users.noreply.github.com>
2021-04-05 13:50:22 +0530

    Update APi.py

--- APi.py
+++ APi.py
@@ -1,10 +1,10 @@
 import twitter
 import csv
-api_key="Kua41JGvLGDFGwSdEFPImiM67"
-api_secret_key="XhCr36YMuK0jrgu34oLMrTnvYTqDznQazzEaEi4DLXp9Ne28Bk"
-bearer_token="AAAAAAAAAAAAAAAAAAAAABEeNAEAAAAAkVKTlVQaQq6KASnV%2F6L5QyzrLDU%3DQ4yjfbWX6Is3VT594rNpRHnZuAG09NaMY3YyIw1AGzSeAvubo3"
-access_token="1266645527917211649-AG6as3XTuTAj2qpISQoYJV8duZZjDJ"
-access_token_secrets="d5ujUWla5zIatoghRhOCRSbqAXqrXTAvc9yrjUYLinBsd"
+api_key=""
+api_secret_key=""
+bearer_token=""
+access_token=""
+access_token_secrets=""
 api = twitter.Api(consumer_key=api_key,consumer_secret=api_secret_key,access_token_key=access_token,access_token_secret=access_token_secrets)
 # print(api.VerifyCredentials())
 # statuses = api.GetUserTimeline(1266645527917211649)
