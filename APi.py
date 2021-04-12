172b084bc893a5d690533cd7db2e29170a87b3f0
Kshitij-Dwivedi <68234512+Kshitij-Dwivedi@users.noreply.github.com>
2021-04-12 21:27:25 +0530

    Update APi.py

--- APi.py
+++ APi.py
@@ -9,13 +9,21 @@ api = twitter.Api(consumer_key=api_key,consumer_secret=api_secret_key,access_tok
 # print(api.VerifyCredentials())
 # statuses = api.GetUserTimeline(1266645527917211649)
 # print([s.text for s in statuses])
-tweets=api.GetSearch(term="Australia", since=2020-11-18,count=10)
+# tweets=api.GetSearch(term="covid", since=2020-11-18,count=10)
+lat=input(" Latitiude ")
+lon=input(" Longitude ")
+rad=input(" Radius ")
+query=lat+','+lon+','+rad+"mi"
+cou=int(input("Number of tweets"))
+ter=input(" Related term ")
+dat=input(" Date should be formatted as YYYY-MM-DD. ")
+tweets=api.GetSearch(geocode=query,lang='en',count=cou,term=ter,since=dat)
 # tweets.text.encode("utf-8")
 a=[t.text for t in tweets]
 # a.encode("utf-8")
 # print(a)
 fields = ['Name']
-with open('tweets.csv','w+',newline='',encoding='utf-8') as f:
+with open('tweets.csv','a',newline='',encoding='utf-8') as f:
     write = csv.writer(f) 
     # write.writerow(fields) 
     for sen in a:
